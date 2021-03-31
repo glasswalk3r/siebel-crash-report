26a658ce453fea8ad983500063c283f21f2ddc8a
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-03-31 16:57:50 -0300

    Revision due security issue
    The pyyaml dependency has a security issue, forcing a version that does 
    not have such bug.
    Removed support of Python 2.x.
    Removed support for Python 3.5.x.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', 'r') as fh:
 
 setup(
     name="siebel-crash-report",
-    version="0.0.1",
+    version="0.0.2",
     author="Alceu Rodrigues de Freitas Junior",
     author_email="glasswalk3r@yahoo.com.br",
     url="https://github.com/glasswalk3r/siebel-crash-report",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
-        'simplejson>=3.13.2',
-        'pyyaml>=3.13'
+        'simplejson>=3.17.2',
+        'pyyaml>=5.4'
     ],
     description='Searchs and aggregates Siebel component crashes information \
 into a nice report.',
@@ -22,10 +22,13 @@ into a nice report.',
     scripts=['bin/crash_reporter'],
     keywords="Siebel crash core dump component",
     classifiers=[
-        "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Operating System :: POSIX :: Linux",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
